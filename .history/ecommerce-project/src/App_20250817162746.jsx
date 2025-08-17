c14e66c51c456b0f6be51500a19e90d1efbc8ba5
darae1225 <darae01225@gmail.com>
2025-08-17 17:03:55 +0800

    Create multiple pages using react-router

---
+++ .history/ecommerce-project/src/App_20250817162746.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+
+function App() {
+  return;
+  <Routes>
+    <Route path="/" element={<HomePage />}></Route>
+  </Routes>;
+}
+
+export default App;
--- .history/ecommerce-project/src/App_20250817162844.jsx
+++ .history/ecommerce-project/src/App_20250817162844.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="/checkout" element={<div>test checkout</div>}></Route>
     </Routes>
   );
 }
