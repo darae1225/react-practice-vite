54e9bc9d21eea79d558bdfaf0d2a9cb588d5f344
darae1225 <darae01225@gmail.com>
2025-08-18 10:44:24 +0800

    Fetch the data from backend

--- .history/ecommerce-project/src/pages/HomePage_20250818102441.jsx
+++ .history/ecommerce-project/src/pages/HomePage_20250818102441.jsx
@@ -4,19 +4,9 @@ import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
-  // fetch("http://localhost:3000/api/products").then((response) => {
-  //   response.json().then((data) => {
-  //     console.log(data);
-  //   });
-  // });
-
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  fetch("http://localhost:3000/api/products").then((response) => {
+    console.log(response.json);
+  });
 
   return (
     <>
--- .history/ecommerce-project/src/pages/HomePage_20250818102521.jsx
+++ .history/ecommerce-project/src/pages/HomePage_20250818102521.jsx
@@ -4,19 +4,11 @@ import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
-  // fetch("http://localhost:3000/api/products").then((response) => {
-  //   response.json().then((data) => {
-  //     console.log(data);
-  //   });
-  // });
-
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
+  fetch("http://localhost:3000/api/products").then((response) => {
+    response.json().then((data) => {
       console.log(data);
     });
+  });
 
   return (
     <>
