ee682be493dff75392fd1dd4754296c8d79e561c
darae1225 <darae01225@gmail.com>
2025-08-18 09:10:36 +0800

    Lesson 6 - exercises

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import "./checkout-header.css";
+import "./CheckoutHeader.css";
 import "./CheckoutPage.css";
 export function CheckoutPage() {
   return (
@@ -7,17 +7,17 @@ export function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/">
+            <Link to="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
             Checkout (
-            <a className="return-to-home-link" href="/">
+            <Link className="return-to-home-link" to="/">
               3 items
-            </a>
+            </Link>
             )
           </div>
 
